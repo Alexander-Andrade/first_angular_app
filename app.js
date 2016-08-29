78a428f9f626a8bc2c7ff7331601092d8ebf00fd
Alexander-Andrade <veimar.94@gmail.com>
2016-08-29 13:57:21 +0300

    watchers...

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-var myApp = angular.module('myApp', ['ngMessages','ngResource']);
+var myApp = angular.module('myApp', []);
 
 
-myApp.controller('mainController', function($scope, $timeout, $filter,  $log) {
+myApp.controller('mainController', function($scope, $timeout, $filter, $log) {
     $scope.handle = '';
     $scope.lowercasehandle = function(){
         return $filter('lowercase')($scope.handle);
     }
+    
+//    setTimeout(function() {
+//       $scope.$apply(function() {
+//           $scope.handle = 'qwerty';
+//           console.log('Scope Changed!');
+//       });
+//        
+//    }, 3000);
+    
+    $timeout(function(){
+        $scope.handle = 'qwerty';
+        console.log('Scope Changed!!!');
+    }, 3000);
 });
