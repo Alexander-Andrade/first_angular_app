9a0ad91b19e36dc68d422229d1fb8ebff62eee09
Alexander-Andrade <veimar.94@gmail.com>
2016-08-30 12:39:47 +0300

    routing-templates

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
+myApp.config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    .when('/second',{
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+});
 
-myApp.controller('mainController', function($scope) {
- 
+myApp.controller('mainController', function($scope, $location, $log){
+    
+});
+
+myApp.controller('secondController', function($scope, $location, $log){
+    
 });
